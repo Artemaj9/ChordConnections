7bb3212455a1d67ccfd8b40751c51061d3fc5bd8
Artem <moiseev@physics.msu.ru>
2023-07-16 01:42:22 +0300

    Initial Commit

---
+++ ChordConnections/ChordConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChordConnectionsApp.swift
+//  ChordConnections
+//
+//  Created by Artem on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChordConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
